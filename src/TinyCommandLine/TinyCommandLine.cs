fac166dba8dd52161fbc2291dd1dc353f63c57b8
IEVin <i.e.vin@ya.ru>
2020-10-31 01:35:59 +0300

    Refactor Option and OptionList function overloads

--- src/TinyCommandLine/TinyCommandLine.cs
+++ src/TinyCommandLine/TinyCommandLine.cs
@@ -455,66 +455,55 @@ namespace System.TinyCommandLine
 
         #region Option(out T value)
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder Option<T>(this CommandBuilder builder, string longName, out T value, string helpText)
-            => builder.Option(NoShortName, longName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder Option<T>(this CommandBuilder builder, char shortName, string longName, out T value, string helpText)
+            => builder.Option(shortName, longName, out value, SetHelpText<T>(helpText));
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder Option<T>(this CommandBuilder builder, string longName, out T value, OptionConfigurator<T> configure = null)
-            => builder.Option(NoShortName, longName, out value, configure);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder Option<T>(this CommandBuilder builder, char shortName, out T value, string helpText)
-            => builder.Option(shortName, NoLongName, out value, b => b.HelpText(helpText));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CommandBuilder Option<T>(this CommandBuilder builder, char shortName, out T value, OptionConfigurator<T> configure = null)
             => builder.Option(shortName, NoLongName, out value, configure);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder Option<T>(this CommandBuilder builder, out T value, string helpText)
-            => builder.Option(NoShortName, NoLongName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder Option<T>(this CommandBuilder builder, char shortName, out T value, string helpText)
+            => builder.Option(shortName, NoLongName, out value, SetHelpText<T>(helpText));
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static CommandBuilder Option<T>(this CommandBuilder builder, string longName, out T value, OptionConfigurator<T> configure = null)
+            => builder.Option(NoShortName, longName, out value, configure);
+
+        public static CommandBuilder Option<T>(this CommandBuilder builder, string longName, out T value, string helpText)
+            => builder.Option(NoShortName, longName, out value, SetHelpText<T>(helpText));
+        
         public static CommandBuilder Option<T>(this CommandBuilder builder, out T value, OptionConfigurator<T> configure = null)
             => builder.Option(NoShortName, NoLongName, out value, configure);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder Option<T>(this CommandBuilder builder, char shortName, string longName, out T value, string helpText)
-            => builder.Option(shortName, longName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder Option<T>(this CommandBuilder builder, out T value, string helpText)
+            => builder.Option(NoShortName, NoLongName, out value, SetHelpText<T>(helpText));
 
         #endregion
 
         #region OptionList(out IReadOnlyList<T> value)
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder OptionList<T>(this CommandBuilder builder, string longName, out IReadOnlyList<T> value, string helpText)
-            => builder.OptionList(NoShortName, longName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder OptionList<T>(this CommandBuilder builder, char shortName, string longName, out IReadOnlyList<T> value, string helpText)
+            => builder.OptionList(shortName, longName, out value, SetHelpText<IReadOnlyList<T>>(helpText));
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder OptionList<T>(this CommandBuilder builder, string longName, out IReadOnlyList<T> value, OptionConfigurator<IReadOnlyList<T>> configure = null)
-            => builder.OptionList(NoShortName, longName, out value, configure);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder OptionList<T>(this CommandBuilder builder, char shortName, out IReadOnlyList<T> value, string helpText)
-            => builder.OptionList(shortName, NoLongName, out value, b => b.HelpText(helpText));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CommandBuilder OptionList<T>(this CommandBuilder builder, char shortName, out IReadOnlyList<T> value, OptionConfigurator<IReadOnlyList<T>> configure = null)
             => builder.OptionList(shortName, NoLongName, out value, configure);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder OptionList<T>(this CommandBuilder builder, out IReadOnlyList<T> value, string helpText)
-            => builder.OptionList(NoShortName, NoLongName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder OptionList<T>(this CommandBuilder builder, char shortName, out IReadOnlyList<T> value, string helpText)
+            => builder.OptionList(shortName, NoLongName, out value, SetHelpText<IReadOnlyList<T>>(helpText));
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static CommandBuilder OptionList<T>(this CommandBuilder builder, string longName, out IReadOnlyList<T> value, OptionConfigurator<IReadOnlyList<T>> configure = null)
+            => builder.OptionList(NoShortName, longName, out value, configure);
+
+        public static CommandBuilder OptionList<T>(this CommandBuilder builder, string longName, out IReadOnlyList<T> value, string helpText)
+            => builder.OptionList(NoShortName, longName, out value, SetHelpText<IReadOnlyList<T>>(helpText));
+
         public static CommandBuilder OptionList<T>(this CommandBuilder builder, out IReadOnlyList<T> value, OptionConfigurator<IReadOnlyList<T>> configure = null)
             => builder.OptionList(NoShortName, NoLongName, out value, configure);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CommandBuilder OptionList<T>(this CommandBuilder builder, char shortName, string longName, out IReadOnlyList<T> value, string helpText)
-            => builder.OptionList(shortName, longName, out value, b => b.HelpText(helpText));
+        public static CommandBuilder OptionList<T>(this CommandBuilder builder, out IReadOnlyList<T> value, string helpText)
+            => builder.OptionList(NoShortName, NoLongName, out value, SetHelpText<IReadOnlyList<T>>(helpText));
 
         #endregion
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static OptionConfigurator<T> SetHelpText<T>(string text) => x => x.HelpText(text);
     }
 }
